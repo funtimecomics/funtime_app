239bc03d187df080b9d2a1513e224672686d5789
Isaac Freeman <isaac@leftclick.com>
2013-11-10 23:05:54 +1300

    adjusted paperclip settings for Sydney AWS server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,7 @@ FuntimeApp::Application.configure do
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :s3_alias_url => 's3-ap-southeast-2.amazonaws.com'
   }
 end
