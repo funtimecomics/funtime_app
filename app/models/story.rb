6ed6256e62b3582547fc86530561a3759ee5a992
Isaac Freeman <isaac@leftclick.com>
2013-12-31 15:53:47 +1300

    Added attr_accessible for story cover image attributes

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
-  attr_accessible :cover_image, :name, :story_pages, :story_pages_attributes, :person_ids
+  attr_accessible :cover_image, :cover_image_file_name, :cover_image_content_type, :cover_image_file_size, :cover_image_updated_at, :name, :story_pages, :story_pages_attributes, :person_ids
   has_attached_file :cover_image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/default_images/story/:style/default.png"
   has_many :pages
   has_and_belongs_to_many :people
