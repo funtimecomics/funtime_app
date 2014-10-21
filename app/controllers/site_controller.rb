12b2e55fffa7b5cd6894d8aacc56d734acd0bd87
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-10-21 22:00:02 +1300

    Site controller shows most recent stories as featured

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,9 +2,9 @@ class SiteController < InheritedResources::Base
   # GET /pages
   # GET /pages.json
   def index
-    @recent_stories = Story.with_pages.alphabetical.recent(4)
-    @stories = Story.with_pages.alphabetical.recent(24).offset(4)
-    @people = Person.alphabetical.recent(24)
+    @recent_stories = Story.with_pages.recent(4)
+    @stories = Story.with_pages.recent(24).offset(4).alphabetical
+    @people = Person.recent(24).alphabetical
 
     respond_to do |format|
       format.html # index.html.erb
