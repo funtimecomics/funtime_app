7eea91ab1cc4211f3fd8299a381eebd430427451
Isaac Freeman <isaac@resolvedigital.co.nz>
2014-11-15 19:49:17 +1300

    Changed secret token to env variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-FuntimeApp::Application.config.secret_token = '43b361c06181fae043e312138adb8a92126ce3db12baab5453337262e570141843b7784f04eb87e7a57c06aa948c45912324fac685ed68267f8da065bbc51013'
+FuntimeApp::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
